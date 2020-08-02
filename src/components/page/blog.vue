748acedcb2e49c1df86fef197243ff67a9cdcde8
sriram23 <sriramsubramanian23@gmail.com>
2020-08-02 20:15:43 +0530

    Added Loader

--- src/components/page/blog.vue
+++ src/components/page/blog.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="blog-container">
+        <loading :active.sync="isLoading" 
+        :can-cancel="false" 
+        :is-full-page="fullPage"></loading>
         <div class="blog-card" v-for="blog in blogs" :key="blog._id">
             <BlogCard @cardClicked="navigateTo(blog)" :title="blog.title" :author="blog.author" :desc="blog.content" :date="convertTime(blog.createdAt)"/>
         </div>
@@ -8,16 +11,20 @@
 
 <script>
 import moment from 'moment'
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 import BlogCard from './blog-card'
 import { mapActions, mapState } from 'vuex'
 
 export default {
     name: 'Blog',
     components: {
-        BlogCard
+        BlogCard,
+        Loading,
     },
     data() {
         return{
+            isLoading: true,
         }
     },
     computed: {
@@ -31,7 +38,9 @@ export default {
             this.$router.push(`/blog/${blog._id}/${blog.title}`);
         },
         async renderBlogs() {
+            this.isLoading = true;
             await this.fetchBlog();
+            this.isLoading = false;
         },
         convertTime(isoTime) {
             const time = moment(isoTime).fromNow();
@@ -40,7 +49,6 @@ export default {
     },
     mounted() {
         this.renderBlogs();
-        console.log('Blogs: ',this.blogs);
     }
 }
 </script>
